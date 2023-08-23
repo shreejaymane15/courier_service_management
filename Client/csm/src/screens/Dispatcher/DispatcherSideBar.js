62b38ec3bc3f91e63ca804374e6e6544da539468
bhosaleshivanjali51 <bhosaleshivanjali51@gmail.com>
2023-08-23 19:28:00 +0530

    test2 commited

--- Client/csm/src/screens/Dispatcher/DispatcherSideBar.js
+++ Client/csm/src/screens/Dispatcher/DispatcherSideBar.js
@@ -1,6 +1,37 @@
-function DispatcherSideBar() {
+function DispatcherSideBar({toggleComponent}) {
+
+  const toggleDispatcherDashboard = () => {
+    toggleComponent("DispatcherDashboard");
+  };
+
+  const toggleOrderSummary = () => {
+    toggleComponent("OrderSummary");
+  };
+
+  const toggleNewOrder = () => {
+    toggleComponent("NewOrder");
+  };
+
+  const toggleDispatcherEmployeeDirectory = () => {
+    toggleComponent("DispatcherEmployeeDirectory");
+  };
+
+  const toggleRaisedTickets = () => {
+    toggleComponent("RaisedTickets");
+  };
+
+  const toggleTracking = () => {
+    toggleComponent("Tracking");
+  };
+
+  const toggleSettings = () => {
+    toggleComponent("Settings");
+  };
+  
+
     return (<>
-        <div className="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+        
+        <div className="sidebar border border-right col-md-12 p-0 bg-body-tertiary">
       <div
         className="offcanvas-lg offcanvas-end bg-body-tertiary"
         tabIndex={-1}
@@ -19,52 +50,60 @@ function DispatcherSideBar() {
             aria-label="Close"
           />
         </div>
-        <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+       
+        <div className="offcanvas-body d-md-flex flex-column overflow-y-auto">
+
           <ul className="nav flex-column">
+          <li className="nav-item">
+            <a
+              className="nav-link d-flex align-items-center gap-2 active"
+              aria-current="page"
+              onClick={toggleDispatcherDashboard}
+            >
+              Dashboard
+            </a>
+          </li>
             <li className="nav-item">
               <a
-                className="nav-link d-flex align-items-center gap-2 active"
-                aria-current="page"
-                href="#"
+               
+                className="nav-link d-flex align-items-center"
+                
+                onClick={toggleOrderSummary}
               >
                Order Summary
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link d-flex align-items-center gap-2" href="#">
+              <a className="nav-link d-flex align-items-center" onClick={toggleNewOrder}>
                 Create New Order
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link d-flex align-items-center gap-2" href="#">
+              <a className="nav-link d-flex align-items-center" onClick={toggleDispatcherEmployeeDirectory}>
                 Employee Directory
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link d-flex align-items-center gap-2" href="#">
+              <a className="nav-link d-flex align-items-center"onClick={toggleRaisedTickets}>
                 Raised Tickets
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link d-flex align-items-center gap-2" href="#">
+              <a className="nav-link d-flex align-items-center" onClick={toggleTracking}>
                 Order Tracking
               </a>
             </li>
-            {/* <li className="nav-item">
-              <a className="nav-link d-flex align-items-center gap-2" href="#">
-                Integrations
-              </a>
-            </li> */}
+           
           </ul>
           <hr className="my-3" />
           <ul className="nav flex-column mb-auto">
             <li className="nav-item">
-              <a className="nav-link d-flex align-items-center gap-2" href="#">
+              <a className="nav-link d-flex align-items-center" onClick={toggleSettings}>
                 Settings
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link d-flex align-items-center gap-2" href="#">
+              <a className="nav-link d-flex align-items-center " href="#">
                 Sign out
               </a>
             </li>
---
+++ Server/CSM/CSM/Controllers/TrackingController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSM.Controllers
+{
+    [RoutePrefix("api/Tracking")]
+    public class TrackingController : ApiController
+    {
+        // GET: Tracking
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class TrackingController : ControllerBase
+{
+    private readonly ITrackingService _trackingService; // Interface for your tracking service
+
+    public TrackingController(ITrackingService trackingService)
+    {
+        _trackingService = trackingService;
+    }
+
+    [HttpGet("shipments/{trackingNumber}")]
+    public async Task<IActionResult> GetShipmentDetails(string trackingNumber)
+    {
+        try
+        {
+            var shipment = await _trackingService.GetShipmentDetailsAsync(trackingNumber);
+            if (shipment == null)
+            {
+                return NotFound("Shipment not found.");
+            }
+
+            return Ok(shipment);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"An error occurred: {ex.Message}");
+        }
+    }
+}
