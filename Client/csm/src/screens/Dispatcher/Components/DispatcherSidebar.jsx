680c87552fd9c40c2af72cc2f8f80355a411bc8b
bhosaleshivanjali51 <bhosaleshivanjali51@gmail.com>
2023-08-25 12:25:28 +0530

    Dispatcher modified

---
+++ Client/csm/src/screens/Dispatcher/Components/DispatcherSidebar.jsx
@@ -0,0 +1,117 @@
+function DispatcherSideBar({toggleComponent}) {
+
+    const toggleDispatcherDashboard = () => {
+      toggleComponent("DispatcherDashboard");
+    };
+  
+    const toggleOrderSummary = () => {
+      toggleComponent("OrderSummary");
+    };
+  
+    const toggleNewOrder = () => {
+      toggleComponent("NewOrder");
+    };
+  
+    const toggleDispatcherEmployeeDirectory = () => {
+      toggleComponent("DispatcherEmployeeDirectory");
+    };
+  
+    const toggleRaisedTickets = () => {
+      toggleComponent("RaisedTickets");
+    };
+  
+    const toggleTracking = () => {
+      toggleComponent("Tracking");
+    };
+  
+    const toggleSettings = () => {
+      toggleComponent("Settings");
+    };
+    
+  
+      return (<>
+          
+          <div className="sidebar border border-right col-md-12 p-0 bg-body-tertiary">
+        <div
+          className="offcanvas-lg offcanvas-end bg-body-tertiary"
+          tabIndex={-1}
+          id="sidebarMenu"
+          aria-labelledby="sidebarMenuLabel"
+        >
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="sidebarMenuLabel">
+              Company name
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              data-bs-target="#sidebarMenu"
+              aria-label="Close"
+            />
+          </div>
+         
+          <div className="offcanvas-body d-md-flex flex-column overflow-y-auto">
+  
+            <ul className="nav flex-column">
+            <li className="nav-item">
+              <a
+                className="nav-link d-flex align-items-center gap-2 active"
+                aria-current="page"
+                onClick={toggleDispatcherDashboard}
+              >
+                Dashboard
+              </a>
+            </li>
+              <li className="nav-item">
+                <a
+                 
+                  className="nav-link d-flex align-items-center"
+                  
+                  onClick={toggleOrderSummary}
+                >
+                 Order Summary
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link d-flex align-items-center" onClick={toggleNewOrder}>
+                  Create New Order
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link d-flex align-items-center" onClick={toggleDispatcherEmployeeDirectory}>
+                  Employee Directory
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link d-flex align-items-center"onClick={toggleRaisedTickets}>
+                  Raised Tickets
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link d-flex align-items-center" onClick={toggleTracking}>
+                  Order Tracking
+                </a>
+              </li>
+             
+            </ul>
+            <hr className="my-3" />
+            <ul className="nav flex-column mb-auto">
+              <li className="nav-item">
+                <a className="nav-link d-flex align-items-center" onClick={toggleSettings}>
+                  Settings
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link d-flex align-items-center " href="#">
+                  Sign out
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      </>);
+  }
+  
+  export default DispatcherSideBar;
--- Server/CSM/CSM/Controllers/DispatcherController.cs
+++ Server/CSM/CSM/Controllers/DispatcherController.cs
@@ -14,16 +14,75 @@ namespace CSM.Controllers
         CSMEntities1 db = new CSMEntities1();
 
         [HttpGet]
-        [Route("api/Dispatcher/GetEmployees/{id}")]
-        public async Task<IHttpActionResult> GetEmployees(int id)
+        [Route("api/Dispatcher/GetEmployees")]
+        public async Task<IHttpActionResult> GetEmployees()
         {
             try
             {
                 var employeeDetails = await Task.Run(() => db.User_Info.ToList()
-                                       .Where(User_Info => User_Info.role_id == 3 && User_Info.address == )
+                                       .Where(User_Info => User_Info.role_id == 3 && User_Info.address == "Satara")
                                        .Select(User_Info => User_Info)
                                        .FirstOrDefault());
                 return Ok(employeeDetails);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception for debugging purposes
+                Console.WriteLine(ex + "An error occurred while processing the request.");
+                return InternalServerError(ex);
+            }
+        }
+
+        /*[HttpGet]
+        [Route("api/Dispatcher/GetOrders")]
+        public async Task<IHttpActionResult> GetOrders(String Location)
+        {
+            try
+            {
+                var orders = await Task.Run(() => db.Orders.ToList()
+                             .Where (Order => Order.receiver_address == ));
+                return Ok(orders);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception for debugging purposes
+                Console.WriteLine(ex + "An error occurred while processing the request.");
+                return InternalServerError(ex);
+            }
+        }*/
+
+        [HttpGet]
+        [Route("api/Dispatcher/GetOrders/{id}")]
+        public IHttpActionResult GetOrders(int id)
+        {
+            try
+            {
+                var orders = (from order in db.Orders
+                              join Dispatcher in db.Dispatchers on order.receiver_address equals Dispatcher.hub_location
+                              where Dispatcher.dispatcher_id == id
+                              select order).ToList(); 
+                             
+                return Ok(orders);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception for debugging purposes
+                Console.WriteLine(ex + "An error occurred while processing the request.");
+                return InternalServerError(ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Dispatcher/GetComplaints")]
+        public async Task<IHttpActionResult> GetComplaints()
+        {
+            try
+            {
+                var complaintDetails = await Task.Run(() => db.Complaints.ToList()
+                                       .Where(Complaint => Complaint.role_id == 3 && Complaint.role_id == 4)
+                                       .Select(Complaint => Complaint));
+ 
+                return Ok(complaintDetails);
             }
             catch (Exception ex)
             {
